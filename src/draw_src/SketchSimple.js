b1117ebcc736d11f9901c22250cc4d234be3fe30
Anthony Siu <anthonysiu@gmail.com>
2018-01-21 16:29:54 -0600

    try

--- src/draw_src/SketchSimple.js
+++ src/draw_src/SketchSimple.js
@@ -7,6 +7,7 @@ function setup() {
   window.scrollTo(0,1);
   document.body.style.overflow = 'hidden';
   createCanvas(window.innerWidth, window.innerHeight);
-  background(255);
-  image(bunnyImage, width/2, height/2, width/10, width/10);
+  background(150);
+  //image(bunnyImage, width/2, height/2, width/10, width/10);
+  image(bunnyImage, 100, 100, 40, 40);
 } // end setup
